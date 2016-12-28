a62814e70d468ef91b062b5b0989a7d6f4eda475
Thomas Sojka <thomas.sojka@comsysto.com>
2016-12-28 15:39:16 +0100

    Access Daily Hours in initCmd

--- Main.elm
+++ Main.elm
@@ -6,6 +6,7 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Http
 import Navigation exposing (Location)
+import Task
 
 
 -- Model
@@ -14,8 +15,7 @@ import Navigation exposing (Location)
 type alias Model =
     { location : Location
     , access_token : Maybe String
-    , hours : Maybe (List DailyHours)
-    , who_am_i : Maybe WhoAmI
+    , hours : List DailyHours
     , error : String
     }
 
@@ -29,19 +29,39 @@ init location =
         initCmd =
             case token of
                 Just aToken ->
-                    Http.send WhoAmI <| getUserInfo aToken
+                    Http.toTask (getUserInfo aToken)
+                        |> Task.andThen (\who -> Task.succeed who.user.id)
+                        |> Task.andThen
+                            (\userId ->
+                                Http.toTask
+                                    (getDailyHoursForDateRange
+                                        (toString userId)
+                                        "20161219"
+                                        "20161225"
+                                        aToken
+                                    )
+                            )
 
                 Nothing ->
-                    Cmd.none
+                    Task.fail (Http.BadUrl "No Access Token found.")
     in
-        ({ location = location
-         , access_token = token
-         , hours = Nothing
-         , who_am_i = Nothing
-         , error = ""
-         }
-            ! [ Navigation.modifyUrl "#", initCmd ]
+        ( { location = location
+          , access_token = token
+          , hours = []
+          , error = ""
+          }
+        , Task.attempt getHours initCmd
         )
+
+
+getHours : Result Http.Error (List DailyHours) -> Msg
+getHours result =
+    case result of
+        Ok res ->
+            Hours res
+
+        Err err ->
+            Failed err
 
 
 
@@ -50,8 +70,8 @@ init location =
 
 type Msg
     = LocationChange Location
-    | Hours (Result Http.Error (List DailyHours))
-    | WhoAmI (Result Http.Error WhoAmI)
+    | Hours (List DailyHours)
+    | Failed Http.Error
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -60,27 +80,11 @@ update msg model =
         LocationChange location ->
             ( { model | location = location }, Cmd.none )
 
-        Hours (Ok hours) ->
-            ( { model | hours = Just hours }, Cmd.none )
+        Hours hours ->
+            ( { model | hours = hours }, Cmd.none )
 
-        Hours (Err err) ->
+        Failed err ->
             ( { model | error = toString err }, Cmd.none )
-
-        WhoAmI (Ok who) ->
-            loadHours model who
-
-        WhoAmI (Err err) ->
-            ( { model | error = toString err }, Cmd.none )
-
-
-loadHours : Model -> WhoAmI -> ( Model, Cmd Msg )
-loadHours model who =
-    case model.access_token of
-        Just token ->
-            { model | who_am_i = Just who } ! [ Http.send Hours (getDailyHoursForDateRange (toString who.user.id) "20161219" "20161225" token) ]
-
-        Nothing ->
-            { model | who_am_i = Just who } ! []
 
 
 
@@ -92,9 +96,7 @@ view model =
     case model.access_token of
         Just token ->
             div []
-                [ div [ style [ ( "margin", "1rem" ) ] ] [ renderUserInfo model.who_am_i ]
-                , hr [] []
-                , div [ style [ ( "margin", "1rem" ) ] ] [ renderHours model.hours ]
+                [ div [ style [ ( "margin", "1rem" ) ] ] [ renderHours model.hours ]
                 , hr [] []
                 , div [ style [ ( "margin", "1rem" ) ] ] [ text model.error ]
                 ]
@@ -110,45 +112,12 @@ renderLoginButton =
         ]
 
 
-renderUserInfo : Maybe WhoAmI -> Html Msg
-renderUserInfo info =
-    case info of
-        Just userInfo ->
-            div []
-                [ img [ src <| userInfo.company.base_uri ++ userInfo.user.avatar_url ] []
-                , div []
-                    [ span [] [ text "ID:" ]
-                    , span [] [ text <| toString userInfo.user.id ]
-                    ]
-                , div []
-                    [ span [] [ text "First name: " ]
-                    , span [] [ text userInfo.user.first_name ]
-                    ]
-                , div []
-                    [ span [] [ text "Last name: " ]
-                    , span [] [ text userInfo.user.last_name ]
-                    ]
-                , div []
-                    [ span [] [ text "Admin: " ]
-                    , span [] [ text <| toString userInfo.user.admin ]
-                    ]
-                ]
-
-        Nothing ->
-            div [] []
-
-
-renderHours : Maybe (List DailyHours) -> Html Msg
-renderHours hrs =
-    case hrs of
-        Just hours ->
-            div []
-                [ h2 [] [ text "Hours for the week 2016-12-19 till 2016-12-25 (dates hardcoded)" ]
-                , List.map renderHour hours |> ul []
-                ]
-
-        Nothing ->
-            div [] []
+renderHours : List DailyHours -> Html Msg
+renderHours hours =
+    div []
+        [ h2 [] [ text "Hours for the week 2016-12-19 till 2016-12-25 (dates hardcoded)" ]
+        , List.map renderHour hours |> ul []
+        ]
 
 
 renderHour : DailyHours -> Html Msg
