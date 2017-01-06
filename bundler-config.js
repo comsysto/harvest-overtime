ed5765fddfa3c48d2679172548df9d2c8168fc45
Thomas Sojka <thomas.sojka@comsysto.com>
2017-01-06 17:22:58 +0100

    Add publicPath for gh-pages

--- bundler-config.js
+++ bundler-config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: '[name]-[hash].js',
-    publicPath: '/'
+    publicPath: '/harvest-overtime'
   },
   resolve: {
     extensions: ['', '.js', '.elm']
