34072ea019b8d17f40f99c675e167c86b6a98ac7
Thomas Sojka <thomas.sojka@comsysto.com>
2017-01-23 09:00:33 +0100

    Scale chart for any input domains

--- Chart.elm
+++ Chart.elm
@@ -1,48 +1,65 @@
 module Chart exposing (lineChart)
 
+import Html
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
-import Html
+import List
 
 
-lineChart : List { a | overtime : Float } -> Html.Html msg
 lineChart points =
-    svg [ viewBox "0 0 1810 150" ]
-        (List.indexedMap
-            (\i p ->
-                g []
-                    [ text_
-                        [ x (toString ((toFloat i) * 35.0 + 12.5))
-                        , y
-                            (if p.overtime < 0 then
-                                (toString (150 - p.overtime * 20 - 45 - (abs (p.overtime * 20))))
-                             else
-                                (toString (150 - p.overtime * 20 - 45))
-                            )
-                        , textAnchor "middle"
+    let
+        chartHeight =
+            400
+
+        min =
+            Maybe.withDefault 0 <| List.minimum <| List.map (\p -> p.overtime - p.compensation) points
+
+        max =
+            Maybe.withDefault 0 <| List.maximum <| List.map (\p -> p.overtime - p.compensation) points
+
+        heightScale =
+            scale min max 0 chartHeight
+    in
+        svg [ viewBox ("0 " ++ toString (abs (heightScale min) - 50) ++ " 2100 500") ]
+            (List.indexedMap
+                (\i p ->
+                     g [ class "o-90"]
+                        [ rect
+                            [ x (toString (i * 40))
+                            , y (toString (computeY heightScale chartHeight (p.overtime - p.compensation)))
+                            , width "30"
+                            , height (toString (abs (heightScale (p.overtime - p.compensation))))
+                            , fill "#eee"
+                            , stroke "black"
+                            , strokeWidth "0.2"
+                            , class "dim"
+                            ]
+                            []
+                        , text_
+                            [ x (toString ((toFloat i) * 40.0 + 15))
+                            , y (toString ((computeY heightScale chartHeight (p.overtime - p.compensation)) + (abs (heightScale (p.overtime - p.compensation)) / 2)))
+                            , textAnchor "middle"
+                            ]
+                            [ text (toString (p.overtime - p.compensation)) ]
+                        , text_
+                            [ x (toString ((toFloat i) * 40.0 + 15))
+                            , y (toString (chartHeight + abs (heightScale min) + 50))
+                            , textAnchor "middle"
+                            ]
+                            [ text (toString i) ]
                         ]
-                        [ text ((toString p.overtime) ++ "h") ]
-                    , rect
-                        [ x (toString (i * 35))
-                        , y
-                            (if p.overtime < 0 then
-                                (toString (150 - p.overtime * 20 - 40 - (abs (p.overtime * 20))))
-                             else
-                                (toString (150 - p.overtime * 20 - 40))
-                            )
-                        , width "25"
-                        , height (toString (abs (p.overtime * 20)))
-                        , fill "#eee"
-                        , class "dim"
-                        ]
-                        []
-                    , text_
-                        [ x (toString ((toFloat i) * 35.0 + 12.5))
-                        , y "150"
-                        , textAnchor "middle"
-                        ]
-                        [ text (toString (i + 1)) ]
-                    ]
+                )
+                points
             )
-            points
-        )
+
+
+scale : Float -> Float -> Float -> Float -> (Float -> Float)
+scale domainMin domainMax rangeMin rangeMax input =
+    input * (rangeMax - rangeMin) / (domainMax - domainMin)
+
+
+computeY heightScale chartHeight overtime =
+    if (overtime < 0) then
+        chartHeight
+    else
+        chartHeight - heightScale overtime
